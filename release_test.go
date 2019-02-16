0d4c584433339f671c0ba7440f570b83842423e5
Songmu <y.songmu@gmail.com>
2019-02-16 23:06:25 +0900

    release_test.go

---
+++ release_test.go
@@ -0,0 +1,27 @@
+package godzilla
+
+import "testing"
+
+func TestGitReg(t *testing.T) {
+
+	testCases := []struct {
+		in, out string
+	}{
+		{"git@github.com:Songmu/gauthor.git", "github.com"},
+		{"git://github.com/tokuhirom/Minilla.git", "github.com"},
+		{"https://github.com/motemen/gore.git", "github.com"},
+		{"https://ore.example.com:8877/motemen/gore.git", "ore.example.com:8877"},
+		{"git://git.example.com:8877/tokuhirom/Minilla.git", "git.example.com:8877"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			m := gitReg.FindStringSubmatch(tc.in)
+			if len(m) < 2 {
+				t.Errorf("something went wrong: not matched")
+			}
+			if m[1] != tc.out {
+				t.Errorf("something went wrong")
+			}
+		})
+	}
+}
