7678afad337a356c67a88383c197b374bd00ab5b
Songmu <y.songmu@gmail.com>
2019-03-03 16:20:27 +0900

    introduce Songmu/gokoku

--- new.go
+++ new.go
@@ -1,17 +1,15 @@
 package godzil
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
-	"text/template"
 	"time"
 
+	"github.com/Songmu/gokoku"
 	"golang.org/x/xerrors"
 )
 
@@ -71,44 +69,10 @@ func (ne *new) do() error {
 	}
 	// create project directory and templating
 	prefix := fmt.Sprintf("/testdata/assets/%s", ne.profile)
-	for _, f := range templates.Files {
-		if f.IsDir() {
-			continue
-		}
-		if !strings.HasPrefix(f.Path, prefix) {
-			continue
-		}
-		targetPathTmpl := strings.Replace(f.Path, prefix, projDir, 1)
-		buf := &bytes.Buffer{}
-		if err := template.Must(template.New(f.Path).Parse(targetPathTmpl)).Execute(buf, ne); err != nil {
-			return xerrors.Errorf("failed to scaffold while resolving targetPath %q: %w",
-				targetPathTmpl, err)
-		}
-		targetPath := buf.String()
-		if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
-			return xerrors.Errorf("failed to scaffold while MkdirAll of %q: %w",
-				targetPath, err)
-		}
-		err := func() (rerr error) {
-			log.Printf("Writing %s\n", targetPath)
-			targetF, err := os.Create(targetPath)
-			if err != nil {
-				return err
-			}
-			defer func() {
-				e := targetF.Close()
-				if rerr == nil && e != nil {
-					rerr = e
-				}
-			}()
-			return template.Must(template.New(f.Path+".tmpl").Parse(string(f.Data))).
-				Execute(targetF, ne)
-		}()
-		if err != nil {
-			return xerrors.Errorf("failed to scaffold while templating %q: %w",
-				targetPath, err)
-		}
+	if err := gokoku.Scaffold(templates, prefix, projDir, ne); err != nil {
+		return xerrors.Errorf("failed to scaffold while templating: %w", err)
 	}
+
 	log.Println("% git init && git add .")
 	c := &cmd{outStream: ne.outStream, errStream: ne.errStream}
 	c.git("-C", projDir, "init")
